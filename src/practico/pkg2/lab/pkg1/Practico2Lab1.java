003618a3a9e8a7738f2a55fb801b8b7ea0d0afa6
AnddyPest <mauripestchanker@gmail.com>
2024-08-21 18:43:34 -0300

    Initial commit

---
+++ src/practico/pkg2/lab/pkg1/Practico2Lab1.java
@@ -0,0 +1,17 @@
+
+package practico.pkg2.lab.pkg1;
+
+/**
+ *
+ * @author mauri
+ */
+public class Practico2Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
