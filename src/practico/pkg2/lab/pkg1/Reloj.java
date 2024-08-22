ee92e18e6a20bc8e979c350436885ebb11dbc52a
Máximo quiroga <maxiquiroga2121@gmail.com>
2024-08-22 17:28:28 -0300

    Update Reloj.java

--- src/practico/pkg2/lab/pkg1/Reloj.java
+++ src/practico/pkg2/lab/pkg1/Reloj.java
@@ -1,13 +1,16 @@
 package practico.pkg2.lab.pkg1;
 
 public class Reloj {
-
+    
     String dia;
+    String mes;
     String hora;
     String modelo;
     long numSerie;
 
-    public Reloj(String modelo, long numSerie) {
+    public Reloj(String dia,String hora,String modelo, long numSerie) {
+        this.dia = dia;
+        this.hora = hora;
         this.modelo = modelo;
         this.numSerie = numSerie;
     }
@@ -37,6 +40,13 @@ public class Reloj {
     }
     
     public void limpiarPantalla() {
-        
+        System.out.println("\n\n\n\n\n\n");
     }
+
+    @Override
+    public String toString() {
+        return "Reloj{" + "dia=" + dia + ", hora=" + hora + ", modelo=" + modelo + ", numSerie=" + numSerie + '}';
+    }
+    
+    
 }
